32c8d501eeed1916d2e1cd9da7a67a3ae92a959e
Rommedahl Magana <rommymagana5@gmail.com>
2025-05-18 12:10:43 +0300

    Populated the README file

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -17,7 +17,7 @@ export default function Login() {
     const password = event.target.password.value;
     const category = event.target.category.value;
     const API_URL = process.env.REACT_APP_API_URL;
-
+    console.log(API_URL);
     try {
       // Make a POST request to the login API
       const response = await axios.post(`${API_URL}/api/login`, {
