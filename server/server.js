997f6342c0b75ff63b10f834554f4aa71a439dca
Dev-Romm <rommymagana5@gmail.com>
2026-09-21 00:15:18 +0300

    CORS error

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: [process.env.clientUrl, "http://localhost:5173"] }));
+app.use(cors({ origin: [process.env.clientUrl, "http://localhost:5173", "http://13.53.36.83:5173"] }));
 
 // const __filename = fileURLToPath(import.meta.url);
 // console.log("FILENAME", __filename);
